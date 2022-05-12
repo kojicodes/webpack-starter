c93b1d371c72898c13bdede0cbe24c46e9adb1c5
Fernando Ortega <kojisan.zx@gmail.com>
2022-05-11 22:15:07 -0500

    feat: add babel and production code minimizer configurations

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,70 @@
+const HtmlWebpack = require('html-webpack-plugin');
+const MiniCssExtract = require('mini-css-extract-plugin');
+const CopyWebpack = require('copy-webpack-plugin');
+const CssMinimizer = require('css-minimizer-webpack-plugin');
+const Terser = require('terser-webpack-plugin');
 
 module.exports = {
 
-    mode: 'production',
+    mode: 'development',
     output: {
-        clean: true
+        clean: true,
+        filename: 'main.[contenthash].js'
     },
     module: {
         rules: [
-
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/i,
+                exclude: /styles.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/i,
+                use: [MiniCssExtract.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
         ]
     },
-    optimization: {},
-    plugins: []
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizer(),
+            new Terser()
+        ]
+    },
+    plugins: [
+        new HtmlWebpack({
+            title: 'My webpack app',
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyWebpack({
+            patterns: [
+                { from: './src/assets/', to: 'assets/' }
+            ]
+        })
+    ]
 }
